00669db06f218c2ee001ff9d5aceb52eecd83fcf
Arturo Martinez <artmasa@msn.com>
2021-04-07 14:46:30 -0500

    making sure visibility is removed for test projects with Release config

--- src/DarkLoop.Azure.Functions.Authorize/Properties/Settings.cs
+++ src/DarkLoop.Azure.Functions.Authorize/Properties/Settings.cs
@@ -1,4 +1,6 @@
 ﻿using System.Runtime.CompilerServices;
 
+#if DEBUG
 [assembly: InternalsVisibleTo("DarkLoop.Azure.Functions.Authorize.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+#endif
