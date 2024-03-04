8995c7b5da7e9a18dc75b533b507bda36ba26938
Arturo Martinez <artmasa@msn.com>
2024-03-04 05:53:05 -0600

    moving internals visible to msbuild

--- src/abstractions/Properties/Settings.cs
+++
@@ -1,9 +0,0 @@
-﻿// <copyright file="Settings.cs" company="DarkLoop" author="Arturo Martinez">
-//  Copyright (c) DarkLoop. All rights reserved.
-// </copyright>
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("DarkLoop.Azure.Functions.Authorize")]
-[assembly: InternalsVisibleTo("DarkLoop.Azure.Functions.Authorization.Isolated")]
-[assembly: InternalsVisibleTo("Abstractions.Tests")]
