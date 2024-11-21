2179968c5bdaa4fd29c6b7404d2738bf0cb84c65
xInfinitYz <123937346+xInfinitYz@users.noreply.github.com>
2024-11-21 15:23:05 +0100

    Allow authenticationScheme at AddJwtFunctionsBearer (#72)
    * checkpoint
    
    * restored private method
    
    * applied suggestions
    
    * Update src/abstractions/FunctionsAuthenticationBuilderExtensions.cs
    
    Applied suggestion
    
    Co-authored-by: Arturo Martinez <artmasa@msn.com>
    
    * Removed double code suggestion
    
    * Update .build/release.props
    
    bumping version up
    
    ---------
    
    Co-authored-by: Arturo Martinez <artmasa@msn.com>

--- src/abstractions/FunctionsAuthenticationBuilderExtensions.cs
+++ src/abstractions/FunctionsAuthenticationBuilderExtensions.cs
@@ -16,6 +16,26 @@ namespace Microsoft.Extensions.DependencyInjection
         /// </summary>
         /// <param name="builder">The current authentication builder.</param>
         /// <param name="configureOptions">An action configuring the JWT options for authentication. 
+        /// <param name="authenticationScheme">The authentication scheme to use.</param>"
+        /// <returns>A instance of the <see cref="FunctionsAuthenticationBuilder"/></returns>
+        public static FunctionsAuthenticationBuilder AddJwtFunctionsBearer(
+            this FunctionsAuthenticationBuilder builder, string authenticationScheme, Action<JwtBearerOptions> configureOptions)
+        {
+            if (authenticationScheme.Equals(JwtBearerDefaults.AuthenticationScheme, StringComparison.OrdinalIgnoreCase))
+            {
+                throw new ArgumentException("Bearer scheme cannot be specified as it conflicts with Azure Functions built-in authentication", nameof(authenticationScheme));
+            }
+            
+            builder.AddJwtBearer(authenticationScheme, configureOptions);
+
+            return builder;
+        }
+
+        /// <summary>
+        /// Adds the JWT "FunctionsBearer" scheme to the authentication configuration.
+        /// </summary>
+        /// <param name="builder">The current authentication builder.</param>
+        /// <param name="configureOptions">An action configuring the JWT options for authentication. 
         /// <returns>A instance of the <see cref="FunctionsAuthenticationBuilder"/></returns>
         public static FunctionsAuthenticationBuilder AddJwtFunctionsBearer(
             this FunctionsAuthenticationBuilder builder, Action<JwtBearerOptions> configureOptions)
